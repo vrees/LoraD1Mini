4633d8093934121aa67f5513c92f8e4c34a5eb70
vrees <viktor.rees@gmx.de>
2019-07-02 15:42:58 +0200

    .

--- src/main.cpp
+++ src/main.cpp
@@ -57,7 +57,7 @@ static osjob_t sendjob;
 
 // Schedule TX every this many seconds (might become longer due to duty
 // cycle limitations).
-const unsigned TX_INTERVAL = 60;
+const unsigned TX_INTERVAL = 20;
 
 // Pin mapping for Wemos Lora
 const lmic_pinmap lmic_pins = {
