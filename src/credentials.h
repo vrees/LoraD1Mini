9ab0b76f9cd22333563a0758761c32dd8d1defae
vrees <viktor.rees@gmx.de>
2019-07-02 17:30:15 +0200

    With display

---
+++ src/credentials.h
@@ -0,0 +1,8 @@
+String nodeid = "myNode";  // nodeid is just used for information, not for network access
+
+// node credentials on ttn are in the console
+// https://console.thethingsnetwork.org/applications/%ApplicationID%/devices/%DeviceID%
+
+const char *devAddr = "260110B5";
+const char *nwkSKey = "62B2A4D1AF9E04C59FABF30637EEEA7C";
+const char *appSKey = "9BCA3A5AB4D00D1665F492D65C130D0A";
